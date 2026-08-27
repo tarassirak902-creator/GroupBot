e302b931a62f7a0846bdb7ff1242b4eb9331aab5
tarassirak902-creator <tarassirak902@gmail.com>
2026-08-27 14:28:15 +0700

    Register group rank notifications

--- src/groupbot/main.py
+++ src/groupbot/main.py
@@ -29,6 +29,7 @@ from groupbot.routers import manual_moderation as manual_moderation_module
 from groupbot.routers.admin_hierarchy import create_admin_hierarchy_router
 from groupbot.routers.admin_member_sync import create_admin_member_sync_router
 from groupbot.routers.admin_rank_audit_actions import create_admin_rank_audit_actions_router
+from groupbot.routers.admin_rank_group_notifications import create_admin_rank_group_notifications_router
 from groupbot.routers.admin_rank_target_actions import create_admin_rank_target_actions_router
 from groupbot.routers.admins_display import create_admins_display_router
 from groupbot.routers.advertising import create_advertising_router
@@ -143,6 +144,7 @@ async def main() -> None:
     dp.include_router(create_group_startgroup_router(session_factory))
     dp.include_router(create_group_router(session_factory))
     dp.include_router(create_admin_rank_target_actions_router(session_factory))
+    dp.include_router(create_admin_rank_group_notifications_router(session_factory))
     dp.include_router(create_admin_rank_audit_actions_router(session_factory))
     dp.include_router(create_admin_member_sync_router(session_factory))
     dp.include_router(create_admins_display_router(session_factory))
