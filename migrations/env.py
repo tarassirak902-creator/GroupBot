4ee3adcd57711c0c319c060a7a16d705598767f7
tarassirak902-creator <tarassirak902@gmail.com>
2026-09-16 01:05:23 +0700

    Register manual advertising models in Alembic

--- migrations/env.py
+++ migrations/env.py
@@ -9,6 +9,8 @@ from groupbot.config import get_settings
 from groupbot.models import Base
 from groupbot import addon_models  # noqa: F401
 from groupbot import advertising_models  # noqa: F401
+from groupbot import advertising_manual_models  # noqa: F401
+from groupbot import advertising_mutual_models  # noqa: F401
 from groupbot import entry_models  # noqa: F401
 from groupbot import network_models  # noqa: F401
 from groupbot import payment_models  # noqa: F401
